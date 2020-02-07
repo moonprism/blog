fc6c440302d5cbd00ed5511643614ad5b491c8c7
moonprism <ankicoe@gmail.com>
2020-02-07 10:03:27 +0800

    取消read服务中强制debug

--- read/public/index.php
+++ read/public/index.php
@@ -1,9 +1,4 @@
 <?php
-
-ini_set('display_errors',1);            //错误信息
-ini_set('display_startup_errors',1);    //php启动错误信息
-error_reporting(-1);                    //打印出所有的 错误信息
-
 define('PUB_PATH', __DIR__);
 // app path
 define('APP_PATH', __DIR__ . '/../app/');
