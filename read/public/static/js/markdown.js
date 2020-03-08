efaf4541dc4b790ae79dbd1f85aa8f068f787f61
moonprism <ankicoe@gmail.com>
2020-03-08 18:23:17 +0800

    :sparkles: 新增markdown.js对html代码的嵌入支持

--- read/public/static/js/markdown.js
+++ read/public/static/js/markdown.js
@@ -4,11 +4,7 @@
 function markdown(input){
     let img_cdn = 'https://kicoe-blog.oss-cn-shanghai.aliyuncs.com/';
     let text = input.replace(/(\r\n|\n|\r)/g, "\n")
-                .replace( /&/g, "&amp;" )
-                .replace( /</g, "&lt;" )
-                .replace( />/g, "&gt;" )
-                .replace( /"/g, "&quot;" )
-                .replace( /'/g, "&#39;" );
+                .replace( /&/g, "&amp;" );
     let re = /([\s\S]+?)($|\n\s*\n|$)+/g, m ,_html = "";
     // line element
     let line_reg = function(str){
@@ -22,7 +18,9 @@ function markdown(input){
                     .replace(/\[(.*?)\]\((.*?)\)/g, '<a target="_blank" href="$2">$1</a>')
                     .replace(/\*\*(.+?)\*\*/g, '<b>$1</b>')
                     .replace(/\*(.+?)\*/g, '<i>$1</i>')
-                    .replace(/`(.+?)`/g, '<code class="_c">$1</code>')
+                    .replace(/`(.+?)`/g, function(match, code) {
+                        return '<code class="_c">'+code_reg(code)+'</code>'
+                    })
                     .replace(/~~(.+?)~~/g, '<s>$1</s>')
                     .replace(/&copyk;/g, '`')
                     .replace(/&copyi;/g, '*')
@@ -37,7 +35,10 @@ function markdown(input){
             case 'c':
             break;
         }
-        return str;
+        return str.replace( /</g, "&lt;" )
+                .replace( />/g, "&gt;" )
+                .replace( /"/g, "&quot;" )
+                .replace( /'/g, "&#39;" );
     }
     if ( ( m = /^(\s*\n)/.exec(text) ) != null ) {
         re.lastIndex = m[0].length;
@@ -95,7 +96,7 @@ function markdown(input){
                 i--;
                 _html += block[i]+'</'+tag+'>';
                 block[i] = '';
-            } else if ( (bq = block[i].match( /^&gt;(?:\s|\[(\S+?)\])(.*?)\s*(?:\n|$)/ )) !== null ) {
+            } else if ( (bq = block[i].match( /^>(?:\s|\[(\S+?)\])(.*?)\s*(?:\n|$)/ )) !== null ) {
                 // blockquote
                 if (to_str != "") {
                     _html += "<p>"+to_str+"</p>";
