77fe3264765917775199c2dd7306141ed6f59018
moonprism <ankicoe@gmail.com>
2021-03-25 11:08:53 +0800

    :art: html格式

--- read/app/view/article/list.php
+++ read/app/view/article/list.php
@@ -11,7 +11,10 @@
     <link rel="stylesheet" type="text/css" href="/dist/css/main.min.css">
     <link rel="stylesheet" type="text/css" href="/dist/css/markdown.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Space+Mono&family=Noto+Sans+SC&family=Roboto+Mono&display=swap" rel="stylesheet">
-    <style><?php echo $setting['global_css'] ?? '' ?></style>
+    <style>
+<?php echo $setting['global_css'] ?? '' ?>
+
+    </style>
 </head>
 <body>
 <div class="an" style=" <?php
@@ -31,47 +34,43 @@ if (isset($setting['background_image'])) {
     </div>
 </div>
 <div id="content" class="article-block">
-    <?php if($tag){ ?>
-        <div style="border-color: <?php echo $tag->color ?>"><div class="tag-banner">当前标签<span class="t" style="background-color: <?php echo $tag->color; ?>"><?php echo $tag->name; ?></span>, 文章数<span class="c"><?php echo $tag->count; ?><span></div></div>
-    <?php } ?>
+<?php if($tag){ ?>
+    <div style="border-color: <?php echo $tag->color ?>"><div class="tag-banner">当前标签<span class="t" style="background-color: <?php echo $tag->color; ?>"><?php echo $tag->name; ?></span>, 文章数<span class="c"><?php echo $tag->count; ?><span></div></div>
+<?php } ?>
     <div class="article-list">
-        <?php foreach($article_list as $article){ ?>
-            <div class="article">
-                <h1><a href="/article/id/<?php echo $article->id ?>"><?php echo stripslashes($article->title) ?></a></h1>
+<?php foreach($article_list as $article){ ?>
+        <div class="article">
+            <h1><a href="/article/id/<?php echo $article->id ?>"><?php echo stripslashes($article->title) ?></a></h1>
                 <div class="mark">
                     <svg class="icon i-date" aria-hidden="true">
                         <use xlink:href="#icondate"></use>
                     </svg>
                     <?php echo date('Y.m.d D', strtotime($article->created_time)) ?>
-                    <?php if($article->getTags()){ ?>
-                        <svg class="icon i-tag" aria-hidden="true">
-                            <use xlink:href="#icontag"></use>
-                        </svg>
-                    <?php } ?>
-                    <?php foreach($article->getTags() as $t) { ?>
-                        <span style="border-color:<?php echo $t->color; ?>;color:<?php echo $t->color; ?>">
-            <a href="/article/tag/<?php echo $t->id ?>" style="<?php if ($tag && $t->id == $tag->id) echo 'background-color: inherit;color: inherit;border-color: inherit'; else echo 'background-color: '.$t->color; ?>" class="tag">
-                    <?php echo $t->name ?>
-                </a>
-            </span>
-                    <?php } ?>
+
+<?php if($article->getTags()){ ?>
+                    <svg class="icon i-tag" aria-hidden="true">
+                        <use xlink:href="#icontag"></use>
+                    </svg>
+<?php } ?>
+<?php foreach($article->getTags() as $t) { ?>
+                    <span style="border-color:<?php echo $t->color; ?>;color:<?php echo $t->color; ?>">
+                        <a href="/article/tag/<?php echo $t->id ?>" style="<?php if ($tag && $t->id == $tag->id) echo 'background-color: inherit;color: inherit;border-color: inherit'; else echo 'background-color: '.$t->color; ?>" class="tag"><?php echo $t->name ?></a>
+                    </span>
+<?php } ?>
                 </div>
                 <div class="summary markdown">
-                    <textarea style="display:none"><?php echo $article->summary?></textarea>
+                    <script type='text/html' style="display:none"><?php echo $article->summary?></script>
                 </div>
             </div>
-        <?php } ?>
+<?php } ?>
     </div>
-    <?php include(dirname(__DIR__).'/article/page.php') ?>
+<?php include(dirname(__DIR__).'/article/page.php') ?>
 </div>
-<!-- <div id="footer">
-    <span class="f_en">Powered by<a href="https://github.com/moonprism/kicoephp-src" target="_blank"> kicoephp </a></span>
-</div> -->
 <script src="/dist/js/main.min.js"></script>
 <script src="/dist/js/markdown.min.js"></script>
 <script>
     document.querySelectorAll('.summary').forEach((summary) => {
-        summary.innerHTML = markdown(summary.getElementsByTagName('textarea')[0].value, main_markdown_config);
+        summary.innerHTML = markdown(summary.getElementsByTagName('script')[0].innerText, main_markdown_config);
     });
 </script>
 <script src="//at.alicdn.com/t/font_1747613_gq90x707awu.js"></script>
@@ -79,6 +78,7 @@ if (isset($setting['background_image'])) {
 <script>hljs.initHighlightingOnLoad();</script>
 <script>
 <?php echo $setting['global_js'] ?? ''; ?>
+
 </script>
 <link rel="stylesheet" type="text/css" href="//cdn.staticfile.org/highlight.js/9.18.1/styles/hopscotch.min.css">
 </body>
